3ab0d8814dd7419d1c84b018d2bb8738613a0fd3
lanou <lanou@mac-mini.local>
2016-04-18 19:28:56 +0800

    添加baseModel

---
+++ WYNews/WYNews/BaseModel.m
@@ -0,0 +1,13 @@
+//
+//  BaseModel.m
+//  WYNews
+//
+//  Created by lanou on 16/4/18.
+//  Copyright © 2016年 xiaochen. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@implementation BaseModel
+
+@end
