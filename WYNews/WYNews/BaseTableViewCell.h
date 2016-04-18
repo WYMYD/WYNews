3b358866bd905e30b1b7e7767be390a4723ad3c1
lanou <lanou@mac-mini.local>
2016-04-18 20:11:07 +0800

    新建 baseTVC

---
+++ WYNews/WYNews/BaseTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableViewCell.h
+//  WYNews
+//
+//  Created by lanou on 16/4/18.
+//  Copyright © 2016年 xiaochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTableViewCell : UITableViewCell
+
+@end
